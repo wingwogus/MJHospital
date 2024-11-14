3413eb5c1399148d114a3439aeff9de29841e10d
이재현 <wingwogus@naver.com>
2024-11-14 16:34:12 +0900

    make HospitalUI start Ui is Login

--- src/MJHospital/Login.java
+++ src/MJHospital/Login.java
@@ -1,7 +1,73 @@
 package MJHospital;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Login extends JFrame {
+class Login extends JFrame implements ActionListener {
+    JTextField loginField, passwdField;
+    public Login() {
+        setTitle("로그인");
+        setSize(800,800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
 
+        JPanel logoPanel = new JPanel();
+        logoPanel.setLayout(new BorderLayout());
+        logoPanel.setBackground(Color.LIGHT_GRAY);
+
+        JLabel hospitalLabel = new JLabel("MJ HOSPITAL", new ImageIcon("src/img/mjicon.png"), JLabel.LEFT);
+        hospitalLabel.setFont(new Font("Serif", Font.BOLD, 30));
+        logoPanel.add(hospitalLabel, BorderLayout.WEST);
+
+        add(logoPanel, BorderLayout.NORTH);
+
+        JPanel loginPanel = new JPanel();
+        loginPanel.setLayout(null);
+
+        JLabel imgLabel = new JLabel(new ImageIcon("src/img/mjicon.png"), JLabel.CENTER);
+        JLabel titleLabel = new JLabel("MJ HOSPITAL", JLabel.CENTER);
+        titleLabel.setFont(new Font("Serif", Font.BOLD, 30));
+        JLabel loginLabel = new JLabel("Login");
+        loginField = new JTextField(20);
+        JLabel passwdLabel = new JLabel("Password");
+        passwdField = new JPasswordField(20);
+        JButton loginButton = new JButton("로그인");
+
+        imgLabel.setBounds(250,150,300,100);
+        titleLabel.setBounds(250,250,300,100);
+        loginLabel.setBounds(250, 500, 100, 30);
+        loginField.setBounds(350, 500, 200, 30);
+        passwdLabel.setBounds(250, 550, 100, 30);
+        passwdField.setBounds(350, 550, 200, 30);
+        loginButton.setBounds(250, 600, 140, 30);
+
+        passwdField.addActionListener(this);
+        loginButton.addActionListener(this);
+
+        loginPanel.add(imgLabel);
+        loginPanel.add(titleLabel);
+        loginPanel.add(loginLabel);
+        loginPanel.add(loginField);
+        loginPanel.add(passwdLabel);
+        loginPanel.add(passwdField);
+        loginPanel.add(loginButton);
+
+        add(loginPanel, BorderLayout.CENTER);
+    }
+
+    public void checkLogin(String id, String pass) {
+        if (id.equals("doctor") && pass.equals("1234")) {
+            new HospitalUI().setVisible(true);
+            dispose();
+        } else {
+            JOptionPane.showMessageDialog(null, "로그인 실패");
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        checkLogin(loginField.getText(), passwdField.getText());
+    }
 }
--- src/MJHospital/Main.java
+++ src/MJHospital/Main.java
@@ -7,39 +7,7 @@ import java.awt.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        HospitalUI frame = new HospitalUI();
-        frame.setVisible(true);
-    }
-
-    static class HospitalUI extends JFrame {
-
-        public HospitalUI() {
-            setTitle("Main");
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            setSize(1280, 720); // 필요한 크기로 설정
-            setResizable(false);
-
-            // 상단 패널 생성
-            JPanel topPanel = new JPanel();
-            topPanel.setLayout(new BorderLayout());
-            topPanel.setBackground(Color.LIGHT_GRAY); // 배경색 설정 (선택사항)
-
-            // 병원 로고 및 이름 레이블
-            JLabel hospitalLabel = new JLabel("MJ HOSPITAL", new ImageIcon("src/img/mjicon.png"), JLabel.LEFT);
-            hospitalLabel.setFont(new Font("Serif", Font.BOLD, 30));
-            topPanel.add(hospitalLabel, BorderLayout.WEST);
-
-            // 탭 패널 생성
-            JTabbedPane tabbedPane = new JTabbedPane();
-            tabbedPane.addTab("진료", new Consultation());
-            tabbedPane.addTab("환자 관리", new PatientManagement());
-            tabbedPane.addTab("진료 관리", new ConsultationManagement());
-            tabbedPane.addTab("예약 관리", new ReservationManagement());
-            tabbedPane.addTab("정보 관리", new StaffManagement());
-
-            // 프레임에 패널 추가
-            add(topPanel, BorderLayout.NORTH);
-            add(tabbedPane, BorderLayout.CENTER);
-        }
+        Login login = new Login();
+        login.setVisible(true);
     }
 }
